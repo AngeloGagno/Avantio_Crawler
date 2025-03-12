e867f4e88f848cf58ef18185152b9a4d8981088e
AngeloGagno <angelogagno@gmail.com>
2025-03-12 17:46:59 -0300

    feat:Criando o main da aplicação

--- src/main.py
+++ src/main.py
@@ -3,14 +3,33 @@ from crawler.review_page.airbnb import Accommodation_info
 from crawler.driver import Driver
 from database.query import query
 import time
-def main():
-    driver_instance = Driver().get_driver()
+import pandas as pd
+
+def dataframe(accomodation_instance):
+    data = []
+    df = query()
+    for accommodation in range(0,len(df)):
+        accomodation_instance.review_page(df.loc[accommodation,'id_acc'])
+        time.sleep(5)
+        data.append(accomodation_instance.summarized_accommodation())
+        time.sleep(5)
+    return data
+
+def start_crawler(driver_instance):
     login_instance = Login(driver_instance)
-    accomodation_instance = Accommodation_info(driver_instance)
     login_instance.connecting_page()
     login_instance.username()
     login_instance.password()
-    time.sleep(2)
-    accomodation_instance.review_page(accommodation_id='328164')
-    time.sleep(5)
-    accomodation_instance.summarized_accommodation()
+
+def fetch_all():
+    driver_instance = Driver().get_driver()
+    accomodation_instance = Accommodation_info(driver_instance)
+    start_crawler(driver_instance)
+    return dataframe(accomodation_instance)
+
+def main(data):
+    df = pd.DataFrame(data)
+    df.to_excel('accommodations.xlsx', index=False)
+    
+if __name__ == '__main__':
+    main(fetch_all())
