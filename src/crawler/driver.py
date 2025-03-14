824b6cb9f48035415a7a56cc2763528857cb0eef
AngeloGagno <angelogagno@gmail.com>
2025-03-14 17:54:19 -0300

    refactor:Adicionando a função headless

--- src/crawler/driver.py
+++ src/crawler/driver.py
@@ -1,13 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from crawler.login.frontpage import Login
-from crawler.review_page.airbnb import Accommodation_info
+
 
 class Driver:
     def __init__(self):
         """Instancia o driver do selenium passando suas configurações"""
         options = Options()
-        options.add_argument("--headless")  # Roda sem abrir o navegador (modo invisível)
+        options.add_argument("--headless") # Roda sem abrir o navegador (modo invisível)
         options.add_argument("--disable-gpu")  # Para sistemas Windows
         options.add_argument("--window-size=1920,1080")  # Tamanho da tela virtual (opcional)
         prefs = {"profile.default_content_setting_values.notifications": 2}
