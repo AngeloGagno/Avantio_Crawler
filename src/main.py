d521cd341918370183b7c0890ec455ee85c382b8
AngeloGagno <angelogagno@gmail.com>
2025-03-14 17:55:36 -0300

    feat:Criando verificação de tela para 2 fatores

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from crawler.login.frontpage import Login
-from crawler.review_page.airbnb import Accommodation_info   
+from crawler.login.twoFA import TwoFactorAuthenticator
+from crawler.review_page.review_scrap import AccommodationInfo   
 from crawler.driver import Driver
 from database.query import query
 import time
@@ -11,24 +12,27 @@ def dataframe(accomodation_instance):
     data = []
     df = query()
     for accommodation in range(0,len(df)):
-        accomodation_instance.review_page(df.loc[accommodation,'id_acc'])
+        accomodation_instance.review_page(df.loc[accommodation,'id_acc'],portal='bookingcom')
         time.sleep(5)
         data.append(accomodation_instance.summarized_accommodation())
         time.sleep(5)
     return data
 
 def start_crawler(driver_instance):
-    """Instancia os passos para realização do login"""
-    login_instance = Login(driver_instance)
-    login_instance.connecting_page()
-    login_instance.username()
-    login_instance.password()
+    """Instancia os passos para realização do login e autenticação 2FA se necessário."""
+    Login(driver_instance)
+    two_fa = TwoFactorAuthenticator(driver_instance)
+    time.sleep(2)
+    if two_fa.check_page():
+        two_fa.run_2fa() 
+        time.sleep(1)
+        Login(driver_instance)
 
 def fetch_all() -> list:
     """Faz o login na avantio e verifica o acesso a pagina de cada propriedade,
     seus status de acordo com a classe Accommodation_info """
     driver_instance = Driver().get_driver()
-    accomodation_instance = Accommodation_info(driver_instance)
+    accomodation_instance = AccommodationInfo(driver_instance)
     start_crawler(driver_instance)
     return dataframe(accomodation_instance)
 
