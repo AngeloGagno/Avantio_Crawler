35249df36c458da83b721e9064a97a2d75bfb115
AngeloGagno <angelogagno@gmail.com>
2025-03-20 17:44:31 -0300

    feat:implementação da funcionalidade de envio para o google sheets

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 import pandas as pd
-from execute_crawler import fetch_all
+from execute_crawler import fetch_all,cleaning_portal
+from load_sheets.send_to_sheets import update_sheets
+import os
+from dotenv import load_dotenv
 import json
 
-def main(data:json) -> None:
-    """Envia para o Excel o retorno do Json criado na automação"""
+def main(data:json, sheet:str) -> None:
+    load_dotenv(override=True)
+    """Envia para o Sheets o retorno do Json criado na automação"""
     df = pd.DataFrame(data)
-    df.to_excel('accommodations.xlsx', index=False)
+    update_sheets(os.getenv('SHEETS_ID'),sheet,df)
     
 if __name__ == '__main__':
-    main(fetch_all())
+    main(fetch_all(),cleaning_portal())
