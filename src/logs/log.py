a1348225028dc1742432b0bbc39a7e856a72b876
AngeloGagno <angelogagno@gmail.com>
2025-03-20 17:45:54 -0300

    refactor:alterando o nivel de criticidade do erro

--- src/logs/log.py
+++ src/logs/log.py
@@ -3,11 +3,12 @@ import os
 import sys
 
 class LoggerService:
-    def __init__(self, log_dir='src/logs'):
+    def __init__(self, log_dir='logs/'):
         os.makedirs(log_dir, exist_ok=True)
 
         logger.remove()
-        # Logs em formato log
+
+        # Logs de ERRO (em vermelho)
         logger.add(
             f"{log_dir}/crawler_errors.log",
             rotation="5 MB",
@@ -16,9 +17,22 @@ class LoggerService:
             format="<red>{time:YYYY-MM-DD HH:mm:ss}</red> | "
                    "<level>{level}</level> | "
                    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
-                   "<level>{message}</level>"
+                   "<red>{message}</red>"
         )
-        # Logs em Json
+
+        # Logs de WARNING (em amarelo)
+        logger.add(
+            f"{log_dir}/crawler_errors.log",
+            rotation="5 MB",
+            retention="7 days",
+            level="WARNING",
+            format="<yellow>{time:YYYY-MM-DD HH:mm:ss}</yellow> | "
+                   "<level>{level}</level> | "
+                   "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
+                   "<yellow>{message}</yellow>"
+        )
+
+        # Logs em JSON
         logger.add(
             f"{log_dir}/crawler_errors.json",
             rotation="5 MB",
@@ -30,13 +44,13 @@ class LoggerService:
         sys.excepthook = self.handle_unhandled_exception
         self.logger = logger
 
-    def handle_unhandled_exception(self, exc_type, exc_value):
-            self.logger.error(
-                f"Erro fatal não tratado: {exc_type.__name__} - {exc_value}"
-            )
-            
+    def handle_unhandled_exception(self, exc_type, exc_value, exc_traceback):
+        self.logger.error(
+            f"Erro fatal não tratado: {exc_type.__name__} - {exc_value}"
+        )
+        
     def error(self, message):
         self.logger.error(message)
 
     def warning(self, message):
-        self.logger.warning(self,message)
+        self.logger.warning(message)
